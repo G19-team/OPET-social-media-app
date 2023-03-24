8007ad93d899e7aabacf714cc88bd754afe07da7
krish_odhaviya <krish.206090307083@gmail.com>
2023-03-24 13:55:44 +0530

    3/24/2023 - some configuration in app.json file added icon and splash screen

--- src/Navigation/StackNav.js
+++ src/Navigation/StackNav.js
@@ -5,15 +5,12 @@ import UserRegister_Screen from "../Screens/UserRegister/UserRegister_Screen";
 import OrganizationRegister_Screen from "../Screens/OrganizationRegister/OrganizationRegister_Screen";
 import EditProfile_Screen from "../Screens/EditProfle/EditProfile_screen";
 import DocumentViewer_Screen from "../Screens/DocumentVIewer/DocumentViewer_Screen";
-import Splash_Screen from "../Screens/SplashScreen/Splash_Screen";
 import Suggestion from "../Screens/SuggestionScreen/Suggestion";
 import HomeNav from "./HomeNav";
 
 //libraries for navigation.
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import { useState, useEffect } from "react";
 
 //intializing stack navigatore.
 const Stack = createNativeStackNavigator();
@@ -22,22 +19,9 @@ import React from "react";
 import { moderateScale } from "react-native-size-matters";
 
 const StackNav = () => {
-  const [showSplashscreen, setshowSplashscreen] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setshowSplashscreen(false);
-    }, 3000);
-  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {showSplashscreen ? (
-          <Stack.Screen
-            name={"Splash_Screen"}
-            component={Splash_Screen}
-            options={{ headerShown: false }}
-          />
-        ) : null}
         <Stack.Screen
           name={"Start_Screen"}
           component={Start_Screen}
